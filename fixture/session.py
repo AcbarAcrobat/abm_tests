3228190200ff0f6884966858ec73da976b38e9d0
artlv <you@example.com>
2020-02-13 12:00:47 +0300

    working tests

---
+++ fixture/session.py
@@ -0,0 +1,17 @@
+import requests
+
+url = "http://192.168.0.125/netrisuser_api"
+
+
+class SessionHelper:
+
+    def login_with_data(self):
+        d = {"login": "test_ABM_admin",
+             "password": "test_ABM_admin"}
+        r = requests.post(url + "/login", json=d)
+        assert r.status_code == 200
+
+
+login = SessionHelper()
+login.login_with_data()
+
---
+++ tests/login_with_null.py
@@ -0,0 +1,17 @@
+import requests
+
+url = "http://192.168.0.125/netrisuser_api"
+
+
+class Tests:
+
+    def login_with_null(self):
+        d = {"login": " ",
+             "password": " "}
+        r = requests.post(url + "/login", json=d)
+        assert r.status_code == 401
+        print(r.status_code)
+
+
+login = Tests()
+login.login_with_null()
