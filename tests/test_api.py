7dd1684ae5132b15e32a214b2547f87f4b0ad90d
artlv <you@example.com>
2020-02-14 16:28:22 +0300

    working test refactoring

---
+++ tests/test_api.py
@@ -0,0 +1,50 @@
+import requests
+import json
+from urllib.parse import urljoin
+from truth.truth import AssertThat
+from support import config
+
+
+class TestApi:
+
+    def auth(self, login=config.get("login"), password=config.get("password"), url=config.get("url")):
+        body = {"login": login,
+                "password": password}
+        r = requests.post(url+"/login", headers={"Content-Type": "application/json"},
+                          json=body)
+        return r
+
+    def test_validate_with_token(self, url=config.get("url")):
+        r = self.auth()
+        v = requests.post(url+"/user", json={'token': r.json()['result']['token']})
+        AssertThat(v.json()['result']).ContainsItem("id", 3562)
+        print(v)
+
+    def test_correct_login_with_data(self):
+        r = self.auth()
+        assert r.status_code == 200
+        AssertThat(r.json()['result']).ContainsItem("id", 3562)
+
+    def test_incorrect_login_with_null(self):
+        d = {"login": " ",
+             "password": " "}
+        r = requests.post(config.get('url') + "/login", json=d)
+        assert r.status_code == 401
+        print(r.status_code)
+
+    def test_incorrect_login_with_data(self):
+        d = {"login": "123",
+             "password": "123"}
+        r = requests.post(config.get("url") + "/login", json=d)
+        assert r.status_code == 401
+        print(r.status_code)
+
+    def test_get_user_with_incorrect_token(self):
+        d = {"token": "3AED6F4648F581DEF6645C833DEFFC9B"}
+        r = requests.post(config.get("url") + "/user", json=d)
+        assert r.status_code == 401
+
+    def test_get_user_with_token_null(self):
+        d = {"token": " "}
+        r = requests.post(config.get("url") + "/user", json=d)
+        assert r.status_code == 401
